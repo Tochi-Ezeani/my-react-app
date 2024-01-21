1e692da9353b02c489106e783b53b33cb21e70c8
Barry <ezeanibarry1@gmail.com>
2024-01-21 07:53:38 +0100

    Updated index.js and added tuts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>;
+// const myFirstElement = <h1>Hello React!</h1>;
+const myArray = ['apple', 'banana', 'orange'];
+const myList = myArray.map((item) => <p>{item}</p>);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(myFirstElement);
+root.render(myList);
