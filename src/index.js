0a51a58f2381231db3cd4ed6ada49d5e4e2947b1
Barry <ezeanibarry1@gmail.com>
2024-01-22 15:10:17 +0100

    Some tutorial examples

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // const myFirstElement = <h1>Hello React!</h1>;
-const myArray = ['apple', 'banana', 'orange'];
-const myList = myArray.map((item) => <p>{item}</p>);
+
+// The map() method
+// const myArray = ['apple', 'banana', 'orange'];
+// const myList = myArray.map((item) => <p>{item}</p>);
+
+const myElement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(myList);
+root.render(myElement);
