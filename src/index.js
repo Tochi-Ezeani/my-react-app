26be163e914887c31ed846dcf11cb2e456350bb7
Barry <ezeanibarry1@gmail.com>
2024-01-23 12:03:26 +0100

    Added Car.js module and updated index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Car from './Car.js';
 
 // const myFirstElement = <h1>Hello React!</h1>;
 
@@ -7,19 +8,54 @@ import ReactDOM from 'react-dom/client';
 // const myArray = ['apple', 'banana', 'orange'];
 // const myList = myArray.map((item) => <p>{item}</p>);
 
-const myElement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
+// Previous myElements
+// (
+//   <table>
+//     <tr>
+//       <th>Name</th>
+//     </tr>
+//     <tr>
+//       <td>John</td>
+//     </tr>
+//     <tr>
+//       <td>Elsa</td>
+//     </tr>
+//   </table>
+// );
+/* <h1>I Love JSX!</h1>; */
+/* <h1>React is { 5 + 5 } times better with JSX</h1>; */
+// (
+//   <ul>
+//     <li>Apples</li>
+//     <li>Bananas</li>
+//     <li>Cherries</li>
+//   </ul>
+// );
+// (
+//   <div>
+//     <h1>I am a Header.</h1>
+//     <h1>I am a Header too.</h1>
+//   </div>
+// );
+// (
+//   <>
+//     <p>I am a paragraph.</p>
+//     <p>I am a paragraph too.</p>
+//   </>
+// );
+/* <input type="text" />; */
+
+// const x = 5;
+// const myElement = <h1>{(x < 10) ? "Hello" : "Goodbye"}</h1>;
+
+// function Garage() {
+//   return (
+//     <>
+//       <h1>Who lives in my Garage?</h1>
+//       <Car />
+//     </>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(myElement);
+root.render(<Car />);
