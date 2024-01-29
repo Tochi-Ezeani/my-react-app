9fcd87ad39cb17e3ca89a26a2770b6b8c0188fb8
Barry <ezeanibarry1@gmail.com>
2024-01-29 23:12:20 +0100

    CSS 2

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,7 @@
 import style from './my-style.module.css';
 
 const Car = () => {
-  return <h1 style={style.bigblue}>Hello Car!</h1>;
+  return <h1 className={style.bigblue}>Hello Car!</h1>;
 }
 
 export default Car;
