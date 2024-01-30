2697c92af585c1b843b4f6667b2cbe13a3ed8a13
Barry <ezeanibarry1@gmail.com>
2024-01-30 01:48:49 +0100

    Sass in React

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,5 @@
-import style from './my-style.module.css';
-
 const Car = () => {
-  return <h1 className={style.bigblue}>Hello Car!</h1>;
+  return <h1>Hello Car!</h1>;
 }
 
 export default Car;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Car from './Car.js';
+import './my-sass.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Car />);
